427c0b95c3226f5bc6c003c66f227dbc2966392a
Reznikov Kirill Sergeevich <75656501+kirka50@users.noreply.github.com>
2025-04-12 16:26:53 +0500

    Update AppRoutes.tsx

--- src/pages/AppRoutes.tsx
+++ src/pages/AppRoutes.tsx
@@ -7,7 +7,7 @@ import CreateTrain from "@/pages/CreateTrain.tsx";
 function AppRoutes() {
     return (
         <Routes>
-            <Route path="/GymWorkOutHelper/" element={<LatestTrain />} />
+            <Route path="/" element={<LatestTrain />} />
             <Route path="/CreateTrain" element={<CreateTrain/>}/>
         </Routes>
     )
